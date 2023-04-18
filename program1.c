c8338704f5549f7d006061ec67172ccdcfec39ec
Ragnar <roshanpp20@gmail.com>
2023-04-18 19:50:29 +0530

    2

---
+++ program1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{   
+    printf("Jay Ganesh...\n");
+    printf("Sita Ram ...\n");
+    printf("Jay Bhole.. ...\n");
+    printf("Hari Hari ...\n");
+    printf("Radhe Radhe ...\n");
+}
---
+++ program2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int No1=10;
+    int No2=11;
+    int Ans=0;
+
+    Ans=No1+No2;
+    prontf("%d\n ,Ans");
+    return 0;
+}
