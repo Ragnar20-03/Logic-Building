7378e2fc25e62edc51ea25090d341b465939bd83
Ragnar <roshanpp20@gmail.com>
2023-04-25 19:32:40 +0530

    Layout

--- program6.c
+++ program6.c
@@ -1,6 +1,15 @@
 //  Write A Program which Checks wheather number is even or Odd.
 
+#include <stdio.h>
+#include<stdbool.h> //for bool data type
 
+// //////////////////////////////////
+// Function name : CheckEvenOdd
+// Input :          Integer
+// Output :         Boolean
+// Description :    Checks weather  input is even or odd
+// Author : Roshan Prashant Patil
+// Date :   25/4/2023
 
 int main()  //Entry Point Function
 {
