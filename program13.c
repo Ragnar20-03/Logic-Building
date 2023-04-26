d1e19be2cab6ac7b9139f46411b74fb6e1f7d8d5
Ragnar <roshanpp20@gmail.com>
2023-04-26 20:38:31 +0530

    13

--- program13.c
+++ program13.c
@@ -2,11 +2,16 @@
 
 void Display(int iNo)
 {
-    
+    if(iNo<0)
+    {
+        printf("Please Enter The Positie Number\n");
+        return;
+    }
    for (int iCnt=0; iCnt<iNo; iCnt++)
    {
         printf("HP...\n");
    }
+
 }
 
 int main(void)
