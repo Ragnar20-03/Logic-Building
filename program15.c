7c746a778df26f5b5992e77faac419cd3bd8e23b
Ragnar <roshanpp20@gmail.com>
2023-04-26 20:57:54 +0530

    15

---
+++ program15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void Diaplay(int iNo1)
+{
+
+}
+
+int main()
+{
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+
+    return 0;
+}
