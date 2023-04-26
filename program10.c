47a6692bec476aaf95c67e47d1c9c2477b845e36
Ragnar <roshanpp20@gmail.com>
2023-04-26 20:10:37 +0530

    00

---
+++ program10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int i=0;
+
+//        1      2    3
+    for (i=1; i<=108; i++)
+    {
+        printf("Jay Ganesh...\n");  //4
+    }
+    printf("%d ",i);
+    return 0;
+}
