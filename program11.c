0f21b4a5c49988e22e0f16a052a1619b605ee028
Ragnar <roshanpp20@gmail.com>
2023-04-26 20:15:16 +0530

    11.c

---
+++ program11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Display()
+{
+   
+        printf("HP...\n");
+    
+}
+
+int main(void)
+{
+     for (int iCnt=1;iCnt<=5;iCnt++)
+    {
+        Display();
+    }
+
+    return 0;
+}
