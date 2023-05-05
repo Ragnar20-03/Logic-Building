a6b070912b84015ee4c7a1a23245f1377df62bd6
Ragnar <roshanpp20@gmail.com>
2023-05-06 00:25:38 +0530

    00

--- program31.c
+++ program31.c
@@ -9,6 +9,10 @@ bool CheckPrime(int iNo)
 {
     int iCnt = 0;
 
+    // Time Complexity of Below Loop
+    // input -> 6 then time complexity = 1
+    // input -> 11 then time complexity = n/2 i.e. Condition of ForLoop i.e 11/2=5;
+
     for(iCnt = 2; iCnt <= (iNo/2); iCnt++)
     {
         if((iNo % iCnt) == 0)
