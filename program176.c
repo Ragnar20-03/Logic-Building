64fba7e0c68972eeea8906913bd2e194350949f8
Ragnar <roshanpp20@gmail.com>
2023-05-22 19:52:00 +0530

    Change String

---
+++ program176.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define ERR_NOTFOUND -1
+
+//  Replace '_' instead of WhiteSpace' ' 
+
+void EditString( char *str )
+{
+  while(*str != '\0')
+  {
+    if (*str == ' ')
+    {
+        *str = '_';
+    }
+  str++;
+  }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s" , Arr);
+
+     EditString( Arr );
+
+     printf("String After Editing is : %s\n",Arr);
+
+    return 0;
+}
