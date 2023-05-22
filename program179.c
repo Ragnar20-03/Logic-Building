850817fea2f7ae41f44329cfc8d18322eeed896c
Ragnar <roshanpp20@gmail.com>
2023-05-22 20:14:54 +0530

    Capital To Small -> 181.c

---
+++ program179.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define ERR_NOTFOUND -1
+
+//  Replace 'Capital ' with its Small Letter  
+
+void strlwrX( char *str )
+{
+  while(*str != '\0')
+  {
+    if ((*str >= 'A') && (*str <= 'Z'))
+    {
+        *str = *str + 32;
+    }
+      str++;
+  }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s" , Arr);
+
+     strlwrX( Arr );
+
+     printf("String After Editing is : %s\n",Arr);
+
+    return 0;
+}
---
+++ program180.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define ERR_NOTFOUND -1
+
+//  Replace 'Small ' with its Capital Letter  
+
+void strUprX( char *str )
+{
+  while(*str != '\0')
+  {
+    if ((*str >= 'a') && (*str <= 'z'))
+    {
+        *str = *str - 32;
+    }
+      str++;
+  }
+}
+
+int main()
+{
+    char Arr[20];
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s" , Arr);
+
+     strUprX( Arr );
+
+     printf("String After Editing is : %s\n",Arr);
+
+    return 0;
+}
