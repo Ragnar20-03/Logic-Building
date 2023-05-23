e752ae103943e34e41fedc8ceee1fd36ebf21bb2
Ragnar <roshanpp20@gmail.com>
2023-05-23 19:43:43 +0530

    Check String is equal or not

---
+++ program195.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+
+// Check strings are equal or not
+
+void strcmpx( char *str1 , char *str2 )
+{
+    while ((*src != '\0') && (str2 != 0) && (*str1 != str2) )
+    {
+        str1++;
+        str2++;
+    }
+
+    if ((*str1 == '\0') && (*str2 == '\0'))
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+int main()
+{
+    char Arr[20];
+    char Brr[20];
+    bool bRet = false;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s" , Arr);
+    printf("Enter number of Letters that you want to Coppy\n");
+    scanf("%d",&iNo);
+
+    bRet =  strcmpx( Arr , Brr  );
+
+    if(bRet == true)
+    {
+        printf("Strings Are equal : \n");
+    }
+    else
+    {
+        printf("Strings Are Not  equal : \n");
+
+    }
+
+    
+    return 0;
+}
