3a90da2f0b567d667709f5b37402d7c728f84e07
Ragnar20-03 <roshanpp20@gmail.com>
2023-06-20 19:21:21 +0530

    DL Linked List

--- program269.c
+++ program269.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Node {
-    struct Node * prev;
+typedef struct Node {
+    struct Node * prev;   //#
     int data ;
     struct node * next;
-};
+}NODE , *PNODE , **PPNODE;
+
+int main()
+{
+    PNODE First = NULL:
+
+    return 0;
+}
