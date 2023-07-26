2314596f4b2db138f4074201a4afa284458afb95
Ragnar20-03 <roshanpp20@gmail.com>
2023-07-26 19:29:07 +0530

    :  DataBase Manegement System Using Java

---
+++ Database_Management_System/program524.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Student
+{
+    public int Rno;
+    public String Name ; 
+    public int Age ; 
+    public int Marks;
+
+}
+
+class program524
+{
+    public static void main(String Arg[])
+    {
+
+    }
+}
